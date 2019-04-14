88e77a94e78c4da2dbbf75ac6652253294925b2d
Constantin Nickel <constantin.nickel@gmail.com>
2019-04-14 17:38:59 +0200

    add modio manifest tests

--- src/manifest.rs
+++ src/manifest.rs
@@ -9,21 +9,21 @@ use crate::utils;
 
 pub type ModDependencies = BTreeMap<String, ModDependency>;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct ModioManifest {
     pub game: Game,
     pub mods: Option<ModDependencies>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, PartialEq, Eq, Serialize)]
 #[serde(untagged)]
 pub enum Identifier {
     Id(u32),
     NameId(String),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Game {
     pub id: Identifier,
@@ -72,7 +72,7 @@ impl<'de> Deserialize<'de> for Identifier {
 }
 // }}}
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, PartialEq, Eq, Serialize)]
 #[serde(untagged)]
 pub enum ModDependency {
     Simple(Identifier),
@@ -145,7 +145,7 @@ impl<'de> Deserialize<'de> for ModDependency {
 }
 // }}}
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct DetailedModDependency {
     pub id: Identifier,
@@ -160,6 +160,112 @@ pub fn read(path: &Path) -> ModiomResult<ModioManifest> {
 pub fn parse(content: &str, path: &Path) -> ModiomResult<ModioManifest> {
     toml::from_str(&content)
         .map_err(format_err!(map "failed to parse manifest at `{}`", path.display()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn identifier() {
+        #[derive(Debug, Eq, PartialEq, Deserialize)]
+        struct Test {
+            id: Identifier,
+        }
+        let actual = toml::from_str("id=1");
+        let expected = Test {
+            id: Identifier::Id(1),
+        };
+        assert!(actual.is_ok());
+        assert_eq!(expected, actual.unwrap());
+
+        let actual = toml::from_str(r#"id="game1""#);
+        let expected = Test {
+            id: Identifier::NameId("game1".to_string()),
+        };
+        assert!(actual.is_ok());
+        assert_eq!(expected, actual.unwrap());
+    }
+
+    #[test]
+    fn mod_dependencies() {
+        let mod1 = ModDependency::Simple(Identifier::Id(1));
+        let mod2 = ModDependency::Simple(Identifier::NameId("mod2".to_string()));
+        let mod3 = ModDependency::Detailed(DetailedModDependency {
+            id: Identifier::Id(3),
+            with_dependencies: Some(true),
+            version: None,
+        });
+        let mut expected = ModDependencies::new();
+        expected.insert("mod1".to_string(), mod1);
+        expected.insert("mod2".to_string(), mod2);
+        expected.insert("mod3".to_string(), mod3);
+
+        let raw = r#"
+        mod1 = 1
+        mod2 = "mod2"
+        mod3 = { id = 3, with-dependencies = true }
+        "#;
+
+        let actual = toml::from_str(raw);
+        assert!(actual.is_ok());
+        assert_eq!(expected, actual.unwrap());
+    }
+
+    #[test]
+    fn manifest() {
+        let raw = r#"
+        [game]
+        id = "gameone"
+
+        [mods]
+        mod1 = 1
+        mod2 = "mod2"
+        mod3 = { id = 3, with-dependencies = true }
+
+        [mods.mod4]
+        id = "mod4"
+
+        [mods.mod5]
+        id = "mod5"
+        version = "1.2"
+        "#;
+
+        let mod1 = ModDependency::Simple(Identifier::Id(1));
+        let mod2 = ModDependency::Simple(Identifier::NameId("mod2".to_string()));
+        let mod3 = ModDependency::Detailed(DetailedModDependency {
+            id: Identifier::Id(3),
+            with_dependencies: Some(true),
+            version: None,
+        });
+        let mod4 = ModDependency::Detailed(DetailedModDependency {
+            id: Identifier::NameId("mod4".to_string()),
+            with_dependencies: None,
+            version: None,
+        });
+        let mod5 = ModDependency::Detailed(DetailedModDependency {
+            id: Identifier::NameId("mod5".to_string()),
+            with_dependencies: None,
+            version: Some(Identifier::NameId("1.2".to_string())),
+        });
+        let mut mods = ModDependencies::new();
+        mods.insert("mod1".to_string(), mod1);
+        mods.insert("mod2".to_string(), mod2);
+        mods.insert("mod3".to_string(), mod3);
+        mods.insert("mod4".to_string(), mod4);
+        mods.insert("mod5".to_string(), mod5);
+        let expected = ModioManifest {
+            game: Game {
+                id: Identifier::NameId("gameone".to_string()),
+                with_dependencies: None,
+            },
+            mods: Some(mods),
+        };
+
+        let actual = toml::from_str(raw);
+        assert!(actual.is_ok());
+        assert_eq!(expected, actual.unwrap());
+    }
 }
 
 // vim: fdm=marker
